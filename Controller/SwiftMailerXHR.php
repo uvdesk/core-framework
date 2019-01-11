b5b3d26c7126b96fea4f3a982c41912e8c83781f
papnoisanjeev <sanjeevpapnoi829@webkul.com>
2019-01-11 11:54:47 +0530

    Check for if mailers exist

--- Controller/SwiftMailerXHR.php
+++ Controller/SwiftMailerXHR.php
@@ -12,8 +12,8 @@ class SwiftMailerXHR extends Controller
     public function loadSettingsXHR(Request $request)
     {
         if (true === $request->isXmlHttpRequest()) {
-            $file_content_array = Yaml::parse(file_get_contents(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml'), 6);
-            $listSwiftmailer = $file_content_array['swiftmailer']['mailers'];
+            $file_content_array = $this->getYamlContentAsArray(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml');
+            $listSwiftmailer = isset($file_content_array['swiftmailer']['mailers'])? $file_content_array['swiftmailer']['mailers']:'';
             return new Response(json_encode($listSwiftmailer), 200, ['Content-Type' => 'application/json']);
         } 
         return new Response(json_encode([]), 404);
@@ -22,7 +22,7 @@ class SwiftMailerXHR extends Controller
     public function removeMailer(Request $request)
     {
         $swiftmailerId = $request->query->get('id');
-        $file_content_array = Yaml::parse(file_get_contents(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml'), 6);
+        $file_content_array = $this->getYamlContentAsArray(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml');
         if (isset($file_content_array['swiftmailer']['mailers'])) {
             $swiftmailers = $file_content_array['swiftmailer']['mailers'];
             unset($swiftmailers[$swiftmailerId]);
@@ -30,10 +30,8 @@ class SwiftMailerXHR extends Controller
                 $swiftmailers = null;
             $file_content_array['swiftmailer']['mailers'] = $swiftmailers;
         }
-
-        // Write the content with new swiftmailer details in file
-        $updateFile = file_put_contents(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml', Yaml::dump($file_content_array, 6));
-
+        // Final write the content with new swiftmailer details in file
+        $updateFile = $this->setYamlContent(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml',$file_content_array);
         if($updateFile) {
             $json['alertClass'] = 'success';
             $json['alertMessage'] = 'Success ! Swiftmailer removed successfully.';
@@ -46,11 +44,30 @@ class SwiftMailerXHR extends Controller
         $response->headers->set('Content-Type', 'application/json');
         return $response;
     }
+ 
+    private function setYamlContent($filePath, $arrayContent)
+    {
+        // Write the content with new swiftmailer details in file
+        return file_put_contents($filePath, Yaml::dump($arrayContent, 6));
+    }
+
+    private function getYamlContentAsArray($filePath)
+    {
+        // Fetch existing content in file
+        $file_content = '';
+        if ($fh = fopen($filePath, 'r')) {
+            while (!feof($fh)) {
+                $file_content = $file_content.fgets($fh);
+            }
+        }
+        // Convert yaml file content into array and merge existing swiftmailer and new swiftmailer
+        return Yaml::parse($file_content, 6);
+    }
 
     private function checkExistingSwiftmailer($uniqueId = null, $email = null, $currentswiftmailer =null)
     {
         $isExist = false;
-        $file_content_array = Yaml::parse(file_get_contents(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml'), 6);
+        $file_content_array = $this->getYamlContentAsArray(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml');
         $existingSwiftmailer = $file_content_array['swiftmailer']['mailers'];
 
         if ($existingSwiftmailer) {
@@ -64,9 +81,9 @@ class SwiftMailerXHR extends Controller
         return $isExist;
     }
 
-    private function getSwiftmailerDetails($swiftmailerId)
+    private function getswiftmailerDetails($swiftmailerId)
     {
-        $file_content_array = Yaml::parse(file_get_contents(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml'), 6);
+        $file_content_array = $this->getYamlContentAsArray(dirname(__FILE__, 5) . '/config/packages/swiftmailer.yaml');
         $swiftmailers = $file_content_array['swiftmailer']['mailers'];
 
         if ($swiftmailers && $swiftmailerId) {
