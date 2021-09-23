cdcfec247515a41592f6dd8676acedc53b23395c
Vipin Shrivastava <vipin.shrivastava889@webkul.com>
2021-09-23 14:49:44 +0530

    fix for ticket.thread message given note message

--- Services/EmailService.php
+++ Services/EmailService.php
@@ -412,7 +412,7 @@ class EmailService
             'ticket.id' => $ticket->getId(),
             'ticket.subject' => $ticket->getSubject(),
             'ticket.message' =>  (isset($ticket->createdThread)) ? $ticket->createdThread->getMessage() : '',
-            'ticket.threadMessage' => (isset($ticket->createdThread)) ? $ticket->createdThread->getMessage() : '',
+            'ticket.threadMessage' => (isset($ticket->createdThread) && $ticket->createdThread->getThreadType() != "note") ? $ticket->createdThread->getMessage() : $ticket->currentThread->getMessage(),
             'ticket.tags' => implode(',', $supportTags),
             'ticket.source' => ucfirst($ticket->getSource()),
             'ticket.status' => $ticket->getStatus()->getDescription(),
